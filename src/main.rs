ab4f4d14a7834333d60fdbec67ac04c3154bb193
Aleksei Trifonov <avrong@avrong.me>
2021-08-24 02:22:37 +0300

    Migrate to Axum 0.2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::Mutex;
 
-use axum::prelude::*;
+use hyper;
+use axum::handler::get;
 use axum::response::Json;
 use clap::Clap;
 use lazy_static::lazy_static;
@@ -10,6 +11,7 @@ use lazy_static::lazy_static;
 use bw_generator::dictionary::Dictionary;
 use bw_generator::phrase::Phrase;
 use bw_generator::settings::Settings;
+use axum::Router;
 
 lazy_static! {
     static ref DICTIONARY: Mutex<Option<Dictionary>> = Mutex::new(None);
@@ -20,7 +22,8 @@ async fn main() {
     let settings = Settings::parse();
     let address = SocketAddr::from_str(&settings.address())
         .expect("Invalid address or port to serve on");
-    let app = route("/", get(root));
+    let app = Router::new()
+        .route("/", get(index));
     *DICTIONARY.lock().unwrap() = Some(Dictionary::from_file(&settings.dictionary));
 
     hyper::Server::bind(&address)
@@ -29,7 +32,7 @@ async fn main() {
         .unwrap();
 }
 
-async fn root() -> Json<Phrase> {
+async fn index() -> Json<Phrase> {
     let statement = DICTIONARY.lock().unwrap().as_ref().unwrap().make_statement();
     let phrase = Phrase::new(&statement);
     Json(phrase)
